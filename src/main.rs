6cf60bb0fd961748a1157eca2604b599e4e9def7
kalter2501 <telman.isayev2501@gmail.com>
2023-07-01 16:24:28 +0400

    format again

--- src/main.rs
+++ src/main.rs
@@ -6,29 +6,26 @@ fn main() {
     let mut input = String::new();
     io::stdin().read_line(&mut input).expect("Incorrect input!");
 
-
     let conversion_type = define_conversion_type(&input);
     let value_to_convert = get_f64_from_input(&input);
-    let result = convert(value_to_convert,conversion_type);
+    let result = convert(value_to_convert, conversion_type);
 
     println!("Result is {result:.2} °{conversion_type}",);
 }
 
 fn get_f64_from_input(input: &String) -> f64 {
-
-    let input :String = input
+    let input: String = input
         .chars()
         .filter(|c| c.is_numeric() || c.eq(&'.'))
         .collect();
 
-    input.parse()
-        .expect("Cannot read number")
+    input.parse().expect("Cannot read number")
 }
 
 fn convert(value: f64, conversion_type: &str) -> f64 {
     match conversion_type {
-        "F"|"f" => (value * 1.8) + 32.0,
-        "C"|"c" => (value - 32.0) * 5.0 / 9.0,
+        "F" | "f" => (value * 1.8) + 32.0,
+        "C" | "c" => (value - 32.0) * 5.0 / 9.0,
         _ => {
             panic!("Incorrect value, couldn't convert")
         }
@@ -45,4 +42,3 @@ fn define_conversion_type(input: &String) -> &str {
         panic!("Input must contain C or F at the end!")
     }
 }
-
